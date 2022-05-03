c70780898dd4f6783cfe18a59b4e283c1bad10e8
DarkEnvoy <devcpy@mail.ru>
2022-05-03 22:51:25 +0500

    Refactoring

--- html2reader/updater.py
+++ html2reader/updater.py
@@ -71,6 +71,39 @@ def filter_processed(articles: Iterator[Article]) -> Iterator[Article]:
         yield article
 
 
+def convert_to_fb2(article: Article) -> Path:
+    headers = {"headers": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0"}
+    try:
+        response = requests.get(article.given_url, headers=headers)
+        response.raise_for_status()
+    except (HTTPError, RequestException, requests.ConnectionError) as e:
+        raise ConversionError from e
+    try:
+        tree = fromstring(response.text)
+    except Exception as e:
+        raise ConversionError from e
+    title = slugify(tree.findtext(".//title", str(article.id)))[:30]
+    logger.info('Title: %s', title)
+    text = html2text.html2text(response.text)
+    local_path = Path("./results")
+    os.makedirs(local_path, exist_ok=True)
+    file_name = f"{title}.fb2"
+    md_file = str((local_path / f"{title}.md").resolve())
+    with open(md_file, 'w') as f:
+        f.write(text)
+    local_file = (local_path / file_name).resolve()
+    try:
+        result = subprocess.run(
+            ['ebook-convert', md_file, str(local_file), f'--title={title}'],
+            stdout=subprocess.DEVNULL,
+        )
+        if result.returncode != 0:
+            raise ConversionError(f'Calibre returned code {result.returncode}')
+    except subprocess.SubprocessError as e:
+        raise ConversionError from e
+    return local_file
+
+
 class Updater:
     def __init__(
         self,
@@ -80,7 +113,6 @@ class Updater:
         interval: datetime.timedelta,
     ):
         self._pocket_fetcher = pocket_fetcher
-        self._pocket_client = pocket_client
         self._dropbox_client = dropbox_client
         self._path = path
         self._interval = interval
@@ -109,36 +141,9 @@ class Updater:
             time.sleep(self._interval.total_seconds())
 
     def _process(self, article: Article) -> None:
-        headers = {"headers": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0"}
-        try:
-            response = requests.get(article.given_url, headers=headers)
-            response.raise_for_status()
-        except (HTTPError, RequestException, requests.ConnectionError) as e:
-            raise ConversionError from e
-        try:
-            tree = fromstring(response.text)
-        except Exception as e:
-            raise ConversionError from e
-        title = slugify(tree.findtext(".//title", str(article.id)))[:30]
-        logger.info('Title: %s', title)
-        text = html2text.html2text(response.text)
-        local_path = Path("./results")
-        os.makedirs(local_path, exist_ok=True)
-        file_name = f"{title}.fb2"
-        md_file = str((local_path / f"{title}.md").resolve())
-        with open(md_file, 'w') as f:
-            f.write(text)
-        local_file = str((local_path / file_name).resolve())
-        try:
-            result = subprocess.run(
-                ['ebook-convert', md_file, local_file, f'--title={title}'],
-                stdout=subprocess.DEVNULL,
-            )
-            if result.returncode != 0:
-                raise ConversionError(f'Calibre returned code {result.returncode}')
-        except subprocess.SubprocessError as e:
-            raise ConversionError from e
-        with open(local_file, "rb") as file_output:
+        local_file = convert_to_fb2(article=article)
+        file_name = local_file.name
+        with open(str(local_file), "rb") as file_output:
             try:
                 self._dropbox_client.files_upload(file_output.read(), str((self._path / file_name).resolve()))
             except dropbox.exceptions.DropboxException as e:
