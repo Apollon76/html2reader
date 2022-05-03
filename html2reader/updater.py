3401739a11ede27e0af2351d2abe84dc19a434fb
DarkEnvoy <devcpy@mail.ru>
2022-05-03 23:05:36 +0500

    Refactoring

--- html2reader/updater.py
+++ html2reader/updater.py
@@ -23,6 +23,19 @@ def filter_processed(articles: Iterator[Article]) -> Iterator[Article]:
         yield article
 
 
+class UploadError(Exception):
+    pass
+
+
+def upload_to_dropbox(dropbox_client: dropbox.Dropbox, base_path: Path, local_file: Path) -> None:
+    file_name = local_file.name
+    with open(str(local_file), "rb") as file_output:
+        try:
+            dropbox_client.files_upload(file_output.read(), str((base_path / file_name).resolve()))
+        except dropbox.exceptions.DropboxException as e:
+            raise UploadError from e
+
+
 class Updater:
     def __init__(
         self,
@@ -38,32 +51,27 @@ class Updater:
 
     def run(self) -> None:
         while True:
-            for article in filter_processed(self._pocket_fetcher.fetch_all()):
-                with db_session:
-                    if exists(e for e in DbAttempt if e.pocket_id == article.id):  # type: ignore
-                        attempt = DbAttempt.get(pocket_id=article.id)
-                        if attempt.number >= 3:
-                            continue
-                        attempt.number += 1
-                    else:
-                        DbAttempt(pocket_id=article.id, number=1)
-
-                logger.info("Processing article %s", article)
-                try:
-                    self._process(article)
-                    with db_session:
-                        DbArticle(pocket_id=article.id)
-                    logger.info("article=%s was processed", article)
-                except ConversionError:
-                    logger.exception("Didn't convert article %s", article.id)
-
-            time.sleep(self._interval.total_seconds())
-
-    def _process(self, article: Article) -> None:
-        local_file = convert_to_fb2(article=article)
-        file_name = local_file.name
-        with open(str(local_file), "rb") as file_output:
             try:
-                self._dropbox_client.files_upload(file_output.read(), str((self._path / file_name).resolve()))
-            except dropbox.exceptions.DropboxException as e:
-                raise ConversionError from e
+                for article in filter_processed(self._pocket_fetcher.fetch_all()):
+                    with db_session:
+                        if exists(e for e in DbAttempt if e.pocket_id == article.id):  # type: ignore
+                            attempt = DbAttempt.get(pocket_id=article.id)
+                            if attempt.number >= 3:
+                                continue
+                            attempt.number += 1
+                        else:
+                            DbAttempt(pocket_id=article.id, number=1)
+
+                    logger.info("Processing article %s", article)
+                    try:
+                        local_file = convert_to_fb2(article=article)
+                        upload_to_dropbox(self._dropbox_client, base_path=self._path, local_file=local_file)
+                        with db_session:
+                            DbArticle(pocket_id=article.id)
+                        logger.info("article=%s was processed", article)
+                    except (ConversionError, UploadError):
+                        logger.exception("Didn't convert article %s", article.id)
+
+                time.sleep(self._interval.total_seconds())
+            except Exception:
+                logger.exception('Unexpected error')
